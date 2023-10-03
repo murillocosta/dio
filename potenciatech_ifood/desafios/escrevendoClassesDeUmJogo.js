88f1c2f55434f24763473773b4d2b17d229ca7c0
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2023-10-03 10:48:15 -0300

    feat: classe de herois challenge

---
+++ potenciatech_ifood/desafios/escrevendoClassesDeUmJogo.js
@@ -0,0 +1,33 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar = () => {
+    let ataque = "";
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+    }
+
+    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}.`);
+  };
+}
+
+const sven = new Heroi("Sven", 33, "guerreiro");
+sven.atacar();
+
+const ezalor = new Heroi("Ezalor", 87, "mago");
+ezalor.atacar();
